5955b94aaa72c8a4dbd83ff5ddce52226053c160
Agstarte <aga1999.1@gmail.com>
2021-11-08 19:06:58 +0100

    added classifiers

--- api/app.py
+++ api/app.py
@@ -1,8 +1,23 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, jsonify
+import requests
+
+import classifiers
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Co mówi ksiądz po ślubie informatyka?</p> \
             <p>Pobieranie zakończone!<p>"
+
+
+@app.route("/classify", methods=['POST'])
+def classify():
+    if request.method == 'POST':
+        data = request.get_json()
+        out = dict()
+        for cls in classifiers.classifiers_pkl:
+            out[cls] = classifiers.classify(cls, data)
+
+        return jsonify(out)
